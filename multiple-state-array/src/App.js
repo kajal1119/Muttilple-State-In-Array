f185a6f95a00f9aa0fe00c61b0c9c84e1a8a6816
kajal <kajal2822000@gmail.com>
2023-07-01 17:17:32 +0530

    second Commit

--- multiple-state-array/src/App.js
+++ multiple-state-array/src/App.js
@@ -1,10 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks,setTasks]=useState([
+  {id:1,name:"Kajal",completed:true},
+  {id:2,name:"PM",completed:false},
+  {id:3,name:"Pritam",completed:true}
+])
+const handleDelete=((id)=>{
+  setTasks(tasks.filter(task=>
+    task.id!==id
+  ))
+})
   return (
     <div className="App">
-    <h1>HELLO!!</h1>
+    <h1>Task List</h1>
+   <ul>
+   {tasks.map((task)=>(
+    <li key={task.id}> <span>{task.id} - {task.name}</span>
+    <button onClick={()=>handleDelete(task.id)} className='delete'>Delete</button>
+    </li>
+   ))}
+   </ul>
     </div>
   );
 }
